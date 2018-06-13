00e2e3d98169622fd7142d009983615cca40fb5a
tiotsop01 <36949192+tiotsop01@users.noreply.github.com>
2018-06-13 17:50:29 +0100

    Update baseurlenvvarTest.sh

--- tests/baseurlenvvarTest.sh
+++ tests/baseurlenvvarTest.sh
@@ -7,8 +7,6 @@ cd civicrm-buildkit-docker
 git checkout tiotsop
 docker-compose -p baseurlenvar down -v
 docker volume rm -f `docker volume ls  -q --f name=baseurlenvar*`
-export DOCKER_CLIENT_TIMEOUT=120
-export COMPOSE_HTTP_TIMEOUT=120
 docker-compose -p baseurlenvar up -d
 docker-compose -p baseurlenvar exec -u buildkit civicrm git config --global user.name Test
 docker-compose -p baseurlenvar exec -u buildkit civicrm git config --global user.email test@example.com
